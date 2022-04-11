897dfaf21ebcba5f6d764c82eb7ede657ed5ce5c
elitaaurora <elitaauroraa@gmail.com>
2022-04-11 13:08:09 +0700

    komentar

---
+++ database/migrations/2022_04_11_054932_create_komentars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('komentars', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_destinasi');
+            $table->string('komen');
+            $table->timestamps();
+
+            //Foreign Key
+            $table->foreign('id_destinasi')->references('id_destinasi')->on('destinasis');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('komentars');
+    }
+};
